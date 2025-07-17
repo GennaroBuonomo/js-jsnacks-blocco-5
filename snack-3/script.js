48c51b7a8758710839de0d32be73b328de18be73
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-17 16:11:46 +0200

    faet fine snack 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,7 +1,7 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
-const incrementedNumbers = numbers.map(nun => num +1);
+const incrementedNumbers = numbers.map(num => num + 1);
 
-
+console.log(incrementedNumbers);
 // Risultato: [3, 9, 5, 8, 3, 88]

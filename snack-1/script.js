e7c8bcf7a499af3f78ab6090a3adeb116f27eba1
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-17 16:26:40 +0200

    feat inizio snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,9 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
+let result ='';
+
+names.forEach((name, index) => {
+  result += `'${name}'`;
+})
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
